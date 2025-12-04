cfdd64c68ee52916a6e386389e3db4e698bc3cd0
hal <halt0413@gmail.com>
2025-12-05 02:19:11 +0900

    仮置きのhome画面の作成

---
+++ apps/frontend/src/app/home/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import { HomeContainer } from "../../features/home";
+
+const HomePage: NextPage = () => {
+  return <HomeContainer />;
+}
+
+export default HomePage;
