8c8d67eeb025f58018d344cab07bf7f821f378d8
hsmt-T <ittuyo.hashi244@gmail.com>
2025-12-07 05:47:39 +0900

    Web1Containerコンポーネントを追加し、ビーチ画像を表示する機能を実装

---
+++ apps/frontend/src/app/web/page.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from "next";
+
+import { Web1Container } from '../../features/web/components/WebContainer';
+
+const WebPage: NextPage = () => {
+    return <Web1Container />;
+}
+
+export default WebPage;
